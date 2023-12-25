bec6560aef73e8c8a8dd8f2ab222b8fab6169d8e
vector-10 <duzieblaise10@gmail.com>
2023-12-25 14:38:04 +0100

    update

--- setup.js
+++ setup.js
@@ -5,8 +5,8 @@ const { execSync } = require('child_process');
 
 
 // Now to specify the particular packages and folders that I want created
-const folders = ["controlers", "middleware", "database.js", "models", "routes", "utils"];
-const packagesToInstall = ["express", "body-parser", "dotenv","morgan", "mongodb", "mongoose", "nodemailer", "passport.js", "jsonwebtoken"];
+const folders = ["controllers", "middleware", "database", "models", "routes", "utils"];
+const packagesToInstall = ["express", "axios", "body-parser", "dotenv", "morgan", "mongoose", "nodemailer", "jsonwebtoken"];
 
 // to create folders
 folders.forEach((folder) => {
@@ -16,7 +16,7 @@ folders.forEach((folder) => {
 
 //To install packages
 packagesToInstall.forEach((pkg) => {
-    console.log(`Installing${pkg}...`);
+    console.log(`Installing ${pkg}...`);
     execSync(`npm install ${pkg}`);
 })
 
