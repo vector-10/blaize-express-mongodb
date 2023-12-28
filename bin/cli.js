52e2301d7630e2fc1646c2c12c42893e91472fe5
vector-10 <duzieblaise10@gmail.com>
2023-12-28 11:28:50 +0100

    update

--- bin/cli.js
+++ bin/cli.js
@@ -3,20 +3,23 @@ const { execSync } = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
+// Array containing folder to be created on setup automatically
 const foldersToCreate = ['controllers', 'middleware', 'database', 'models', 'routes', 'utils'];
-const packagesToInstall = [
-  'express',
-  'axios',
-  'body-parser',
-  'dotenv',
-  'morgan',
-  'mongodb',
-  'mongoose',
-  'jsonwebtoken',
-  'cors',
-  'bcrypt'
-];
 
+// call the functions before their declarations(hoisting) to enable readability and collaboration.
+createFolders(foldersToCreate);
+createPackageJSON();
+installPackages();
+createAppJSFile();
+createSampleController();
+createSampleRouter();
+createConnectToDBFile();
+createGitIgnoreFile();
+createDotEnvFile();
+
+console.log('Express API Setup complete!');
+
+// function that handles the creation of folders during the setup
 function createFolders(folders) {
   folders.forEach((folder) => {
     const folderPath = path.join(process.cwd(), folder);
@@ -25,18 +28,52 @@ function createFolders(folders) {
   });
 }
 
-function installPackages(packages) {
+// function that automatically creates the package.json function
+function createPackageJSON() {
+  // Run npm init -y to generate a default package.json
+  execSync('npm init -y', { stdio: 'inherit' });
+  console.log('package.json file created successfully!');
+}
+
+// function to handle npm packages installation
+function installPackages() {
+  const packagesToInstall = [
+    'express',
+    'axios',
+    'body-parser',
+    'dotenv',
+    'morgan',
+    'mongoose',
+    'jsonwebtoken',
+    'nodemon',
+    'cors',
+    'bcrypt'
+  ];
+
   console.log('Installing packages...');
-  execSync(`npm install ${packages.join(' ')} --save`, { stdio: 'inherit' });
+  execSync(`npm install ${packagesToInstall.join(' ')} --save`, { stdio: 'inherit' });
   console.log('Packages installed successfully!');
 }
 
-// default setup for the app.js file to be created by default
+// function incharge of creating of creating files, it takes two arguments. The file path and the content of the file
+function createFile(filePath, content) {
+  const fullPath = path.join(process.cwd(), filePath);
+  fs.writeFileSync(fullPath, content);
+
+  if (fs.existsSync(fullPath)) {
+    console.log(`${filePath} file created successfully`);
+  } else {
+    console.error(`Failed to create ${filePath} file `);
+  }
+}
+
+//function to create app.js file
 function createAppJSFile() {
   const appJSContent = `
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const sampleDataRoute = require('./routes/sampleRoute');
 
 // Middleware
 app.use(bodyParser.json());
@@ -46,6 +83,9 @@ app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
+app.get('/api/v1', sampleDataRoute);
+
+
 // Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
@@ -53,16 +93,65 @@ app.listen(PORT, () => {
 });
   `;
 
-  const appJSPath = path.join(process.cwd(), 'app.js');
-  fs.writeFileSync(appJSPath, appJSContent);
-
-  console.log('app.js file created successfully!');
+  createFile('app.js', appJSContent);
 }
 
+//function to create sample controller file
+function createSampleController() {
+  const sampleControllerContent = 
+  ` const getSampleData = (req, res) => {
+    res.json({ message: " Hello from sample controller " })
 
-// Create folders, installs packages and creates an app.js file
-createFolders(foldersToCreate);
-installPackages(packagesToInstall);
-createAppJSFile();
+    module.exports = { getSampleData }
+  }`
 
-console.log('Express API Setup complete!');
+  createFile('controllers/sampleController.js', sampleControllerContent);
+}
+
+//function to create sample router file
+function createSampleRouter() {
+  const sampleRouterContent = 
+  `const express = require('express');
+   const router = express.Router();
+   const { sampleController } = require('../controllers/sampleController')
+   
+   //Define routes
+   router.route('/sample').get(sampleController.getSampleData).post().put().delete()
+   
+   module.exports = router;`;
+
+   createFile('routes/sampleRoute.js', sampleRouterContent);
+}
+
+//function to create connect.js file
+function createConnectToDBFile() {
+  const connectToDBContent = 
+  ` const mongoose = require("mongoose");
+
+    const connectDB = () => {
+      mongoose.connect()
+        .then((result) => {
+          console.log(
+            \`MongoDB database connected with HOST: \${result.connection.host}\`
+          );
+        });
+    };
+  
+  module.exports = connectDB;`
+
+  createFile('database/connect.js', connectToDBContent)
+}
+
+//function to create gitignore file
+function createGitIgnoreFile() {
+  const gitIgnoreContent = `node_modules/ *.env`;
+
+  createFile('.gitignore', gitIgnoreContent);
+}
+
+//function to create dotenv file
+function createDotEnvFile() {
+  const dotEnvContent = `PORT=5000`;
+
+  createFile('.env', dotEnvContent);
+}
